35a7ef69edfc2b1f43644f989d8a94594222a0e0
Angelinne <kayumi85@hotmail.com>
2022-07-03 18:26:13 +0900

    first commit

---
+++ fdf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 18:59:57 by root              #+#    #+#             */
+/*   Updated: 2022/07/02 21:23:27 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+#include "get_next_line/get_next_line.h"
+#include "libft/libft.h"
+# include <unistd.h>
+# include <stddef.h>
+# include <ctype.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <string.h>
+# include <stdint.h>
+# include <math.h>
+
+# endif
---
+++ main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 19:09:04 by root              #+#    #+#             */
+/*   Updated: 2022/07/03 18:18:03 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int     main(int argc, char **argv)
+{
+	char    *take_out_line;
+	char    **new_map;
+	int     i;
+	int		fd;
+
+	fd = open("test.txt", O_RDONLY);
+	
+	take_out_line = get_next_line(fd);
+
+	i = 0;
+	while(take_out_line[i]  != '\0')
+	{
+		printf("%s\n", take_out_line);
+		take_out_line = get_next_line(fd);
+		i++;
+	}
+}
