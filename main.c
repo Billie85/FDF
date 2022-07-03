9ecb6c36673996123f866ab765471bd8a8ae6f7c
Angelinne <kayumi85@hotmail.com>
2022-07-04 00:50:34 +0900

    new tes.txt

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 19:09:04 by root              #+#    #+#             */
-/*   Updated: 2022/07/03 18:18:03 by root             ###   ########.fr       */
+/*   Updated: 2022/07/04 00:49:14 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int     main(int argc, char **argv)
 	fd = open("test.txt", O_RDONLY);
 	
 	take_out_line = get_next_line(fd);
-
+	
 	i = 0;
 	while(take_out_line[i]  != '\0')
 	{
