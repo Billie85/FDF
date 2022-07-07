0a7f35bff550650a5a56c69c9652dfb1fbcba842
Angelinne <kayumi85@hotmail.com>
2022-07-07 20:59:44 +0900

    add split_line

--- ft_read.c
+++ ft_read.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 22:08:26 by root              #+#    #+#             */
-/*   Updated: 2022/07/07 20:07:14 by root             ###   ########.fr       */
+/*   Updated: 2022/07/07 20:59:15 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,24 @@ void ft_read(char *argv, t_fdf *fdf)
 	char    *take_out_line;
 	char	**split_line;
 	int		fd;
+	size_t	len1;
 
-	fd = open("test.txt", O_RDONLY);
-	take_out_line = get_next_line(fd);
-	
-	int i = 0;
-	while(take_out_line != NULL)
+	fd = open ("test.txt", R_OK);
+	while (take_out_line != NULL)
 	{
+		take_out_line = get_next_line(fd);
 		split_line = ft_split(take_out_line, ' ');
-		printf("%s\n", split_line[i]);
-		//take_out_line = get_next_line(fd);
-		i++;
+		len1 = 0;
+		while(split_line[len1] != NULL)
+		{
+			printf("%s", split_line[len1]);
+			//スペースなしの値が出力される。
+			//1234
+			//5678
+			//9153
+			len1++;
+		}
 	}
-	free(take_out_line);
+	//free(take_out_line);
 	close(fd);
 }
