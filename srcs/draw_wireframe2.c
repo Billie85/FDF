bf81bcd59a36cb4cce6a550f6544355d25672f66
Angelinne <kayumi85@hotmail.com>
2022-09-01 00:40:10 +0900

    norm

--- srcs/draw_wireframe2.c
+++ srcs/draw_wireframe2.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 00:35:49 by root              #+#    #+#             */
-/*   Updated: 2022/08/31 00:38:11 by root             ###   ########.fr       */
+/*   Updated: 2022/09/01 00:39:48 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	width_x(t_fdf *data)
 {
-	if (data->ax.x_f < data->map_width -1)
+	if (data->ax.x_f < data->map_width - 1)
 	{
 		data->ax.x1_f = data->ax.x_f + 1;
 		data->ax.y1_f = data->ax.y_f;
@@ -24,7 +24,7 @@ void	width_x(t_fdf *data)
 
 void	height_y(t_fdf *data)
 {
-	if (data->ax.y_f < data->map_height -1)
+	if (data->ax.y_f < data->map_height - 1)
 	{
 		data->ax.x1_f = data->ax.x_f;
 		data->ax.y1_f = data->ax.y_f + 1;
